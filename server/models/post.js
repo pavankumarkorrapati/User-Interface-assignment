5408c2a7b3cc80488955cc0eef04f2e22bf5b07b
pavankumarkorrapati <korrapatipavankumar2000@gmail.com>
2023-06-14 00:03:46 -0400

    changes in server folder

--- server/models/post.js
+++ server/models/post.js
@@ -1,23 +1,44 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-   
-    comment: { type: String, required: true}
-   
-  })
-  
-  
-  const Post = mongoose.model("Post", postSchema);
-  
+  user: String,
+  comment: String
+})
 
-  async function comment(comment) { 
-    const info = await comment(comment);
-  
-    const newPost = await Post.create({
-     comment: comment
+const Post = mongoose.model("Post", postSchema);
+
+async function produce(user, comment) {
+    const newPost = await Post.produce({
+      user: user,
+      comment: comment
+
     });
   
     return newPost;
   }
 
-  module.exports = {comment};
+
+async function getuser(user) {
+    const post = await getPost(user);
+    if(!post) throw Error('comment not found');
+    return post;
+  }
+
+
+async function updatePost(id, newText) {
+    const post = await post.updateOne({"_id": id}, {$set: { comment: newText}});
+    return post;
+  }
+
+
+async function deletePost(id) {
+    await Post.deleteOne({"_id": id});
+  };
+  
+
+async function getPost(user) {
+    return await Post.findOne({ "user": user});
+  }
+  
+
+  module.exports = {produce, getuser, updatePost, deletePost, getPost};
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,20 +1,39 @@
-
 const express = require("express");
-const Post = require('../models/post'); 
+const Post = require('../models/post');
 const router = express.Router();
 
-
 router
- .post('/comment', async (req, res) => {
-    try {
-      const post = await Post.comment(req.body.comment);
-      res.send({comment:undefined});
-    } catch(error) {
-      res.status(401).send({ message: error.message }); 
+.post('/createpost', async (req,res) => {
+    try{
+     const post = await Post.produce(req.body.user, req.body.comment);  
+     res.send({...post});
+    } catch(error){
+      res.status(401).send({message: error.message});
     }
-  })
+ })  
+ .post('/getuser', async (req,res) => {
+    try{
+     const post = await Post.getuser(req.body.user);  
+     res.send({...post});
+    } catch(error){
+      res.status(401).send({message: error.message});
+    }
+ })
+.put('/update', async (req,res) => {
+    try{
+     const post = await Post.updatePost(req.body.id,req.body.newText);  
+     res.send({...post, password: undefined});
+    } catch(error){
+      res.status(401).send({message: error.message});
+    }
+ })
+ .delete('/delete', async (req,res) => {
+    try{
+     await Post.deletePost(req.body.id);  
+     res.send({ success: "Account successfully Deleted" });
+    } catch(error){
+      res.status(401).send({message: error.message});
+    }
+ })
 
- 
-
-
-module.exports = router;
+ module.exports = router;
